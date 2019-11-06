cd443ad173ec4ef4effae55e9ec4a6f728abc254
Saturnluohao <3099916791@qq.com>
2019-11-06 23:39:21 +0800

    Test webserver

--- fileserver/src/main/java/com/f4/linkage/fileserver/FileserverApplication.java
+++ fileserver/src/main/java/com/f4/linkage/fileserver/FileserverApplication.java
@@ -1,13 +1,22 @@
 package com.f4.linkage.fileserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FileserverApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileserverApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(FileserverApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(FileserverApplication.class, args);
+        String[] names = context.getBeanDefinitionNames();
+        for(int i = 0; i < names.length; i++){
+            LOGGER.info("name " + i + " is " + names[i]);
+        }
     }
 
 }
