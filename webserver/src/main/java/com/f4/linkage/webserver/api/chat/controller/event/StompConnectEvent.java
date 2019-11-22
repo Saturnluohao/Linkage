7e485f23fd112e91470c515e0d350ec5f7e8c6a4
Zijian Zhang <39160610+bulffi@users.noreply.github.com>
2019-11-22 23:47:58 +0800

    User is locked when sign up is fixed!

--- webserver/src/main/java/com/f4/linkage/webserver/api/chat/controller/event/StompConnectEvent.java
+++ webserver/src/main/java/com/f4/linkage/webserver/api/chat/controller/event/StompConnectEvent.java
@@ -1,6 +1,8 @@
 package com.f4.linkage.webserver.api.chat.controller.event;
 
 import com.f4.linkage.webserver.config.OnlineUserHub;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
@@ -15,11 +17,13 @@ import java.util.Objects;
  **/
 
 public class StompConnectEvent implements ApplicationListener<SessionConnectedEvent> {
+  private Logger logger = LoggerFactory.getLogger(StompDisconnectEvent.class);
   @Autowired
   OnlineUserHub onlineUserHub;
 
   @Override
   public void onApplicationEvent(SessionConnectedEvent sessionConnectedEvent) {
+    logger.info(Objects.requireNonNull(sessionConnectedEvent.getUser()).getName()+" is connected!");
     System.out.println(Objects.requireNonNull(sessionConnectedEvent.getUser()).getName()+" is connected!");
     onlineUserHub.userConnect(Objects.requireNonNull(sessionConnectedEvent.getUser()).getName());
   }
--- webserver/src/main/java/com/f4/linkage/webserver/api/chat/controller/event/StompDisconnectEvent.java
+++ webserver/src/main/java/com/f4/linkage/webserver/api/chat/controller/event/StompDisconnectEvent.java
@@ -1,6 +1,8 @@
 package com.f4.linkage.webserver.api.chat.controller.event;
 
 import com.f4.linkage.webserver.config.OnlineUserHub;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
@@ -14,10 +16,13 @@ import java.util.Objects;
  * @create: 2019/10/28
  **/
 public class StompDisconnectEvent implements ApplicationListener<SessionDisconnectEvent> {
+  private Logger logger = LoggerFactory.getLogger(StompDisconnectEvent.class);
+
   @Autowired
   OnlineUserHub onlineUserHub;
   @Override
   public void onApplicationEvent(SessionDisconnectEvent sessionDisconnectEvent) {
+    logger.info(Objects.requireNonNull(sessionDisconnectEvent.getUser()).getName()+" is leaving!");
     System.out.println(Objects.requireNonNull(sessionDisconnectEvent.getUser()).getName()+" is leaving!");
     onlineUserHub.userDisconnect(Objects.requireNonNull(sessionDisconnectEvent.getUser()).getName());
   }
