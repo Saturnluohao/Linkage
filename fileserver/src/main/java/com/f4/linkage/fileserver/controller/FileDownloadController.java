b5e3b1bcb5b3b052a2696271ab73958afb946196
Saturnluohao <3099916791@qq.com>
2019-12-05 18:48:33 +0800

    Fix some bug

--- fileserver/src/main/java/com/f4/linkage/fileserver/controller/FileDownloadController.java
+++ fileserver/src/main/java/com/f4/linkage/fileserver/controller/FileDownloadController.java
@@ -38,8 +38,8 @@ public class FileDownloadController {
     }
 
     @GetMapping("/post/picture/{picture_index}")
-    void postPictureTransferer(@PathVariable int id, @PathVariable int picture_index, HttpServletResponse response){
-        String fileName = "post/img/" + id + "_" + picture_index;
+    void postPictureTransferer(@PathVariable int picture_index, HttpServletResponse response){
+        String fileName = "post/img/" + picture_index + "_" + picture_index;
         fileUtil.transfer(response, fileName, FileKind.MomentPicture);
     }
 
